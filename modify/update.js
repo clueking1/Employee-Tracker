e54ae092d983c16b7385a4741f5416175bd20b1b
Will Coan <wcoan300@gmail.com>
2020-02-13 23:43:44 -0500

    can update employee

--- modify/update.js
+++ modify/update.js
@@ -161,3 +161,198 @@ function updateRole (roleId, ans, low) {
         )
     }
 }
+
+function employees () {
+    begin.con.query(
+        'SELECT * FROM employee', (err, res) => {
+            if (err) {
+                throw err
+            }
+            inquirer.prompt([
+                {
+                type: 'list',
+                name: 'updateEmploy',
+                message: 'What Employee would you like to update',
+                choices: res.map(t => `${t.first_name} ${t.last_name}`)
+                },
+                {
+                    type: 'list',
+                    name: 'updateWhatEm',
+                    message: 'What would you like to update?',
+                    choices: ['First name', 'Last name', 'Role', 'Manager']
+                },
+
+        ])
+            .then(ans => {
+                let chosenMan;
+
+                res.find(t => {
+                    if (`${t.first_name} ${t.last_name}` === ans.updateEmploy) {
+                        chosenMan = t.id
+                    }
+                })
+                if (ans.updateWhatEm === 'Role'){
+                    updateEmRole(chosenMan)
+                } else if (ans.updateWhatEm === 'Manager'){
+                    updateEmManage(chosenMan)
+                } else if (ans.updateWhatEm === 'First name'){
+                    updateEmFirst(chosenMan)
+                } else {
+                    updateEmLast(chosenMan)
+                }
+                
+            })
+        }
+    )
+}
+
+function updateEmRole (personId) {
+    begin.con.query(
+        'SELECT * FROM role', (err, res) => {
+            inquirer.prompt(
+                {
+                type: 'list',
+                name: 'updateEmRole',
+                message: 'What is the new Role for this Employee?',
+                choices: res.map(t => t.title)
+                }
+            )
+            .then(ans => {
+                let chosenRole;
+
+                res.find(t => {
+                    if (t.title === ans.updateEmRole) {
+                        chosenRole = t.id
+                    }
+                    begin.con.query(
+                        'UPDATE employee SET ? WHERE ?',[
+                            {
+                                role_id: chosenRole
+                            },
+                            {
+                                id: personId
+                            }
+
+                        ],
+                        err => {
+                            if (err) {
+                                throw err
+                            }
+                            console.log('Employee Role updated!!!!!')
+                            begin.start()
+                        }
+                    )
+                })
+            })
+        }
+    )   
+}
+function updateEmManage (personId) {
+    begin.con.query(
+        'SELECT * FROM employee', (err, res) => {
+            inquirer.prompt({
+                type: 'list',
+                name: 'whoManages',
+                message: 'Who manages this employee?',
+                choices: res.map(t => `${t.first_name} ${t.last_name}` )
+            })
+            .then(ans => {
+                let chosenManager;
+
+                    res.find(t => {
+                        if (`${t.first_name} ${t.last_name}` === ans.whoManages) {
+                            chosenManager = t.id
+                        }
+                    })
+
+
+                    begin.con.query(
+                        'UPDATE employee SET ? WHERE ?',
+                        [
+                            {
+                                manager_id: chosenManager
+                            },
+                            {
+                                id: personId
+                            }
+                        ],
+                        err => {
+                            if (err) {
+                                throw err
+                            }
+                            console.log('Manager Updated!!!!!')
+                            begin.start()
+                        }
+                    )
+                })
+        }
+    )
+}
+
+function updateEmFirst (personId) {
+    begin.con.query(
+        'SELECT * FROM employee', (err, res) => {
+            inquirer.prompt({
+                type: 'input',
+                name: 'newFirst',
+                message: 'What is the new first name'
+            })
+            .then(ans => {
+                begin.con.query(
+                        'UPDATE employee SET ? WHERE ?',
+                        [
+                            {
+                                first_name: ans.newFirst
+                            },
+                            {
+                                id: personId
+                            }
+                        ],
+                        err => {
+                            if (err) {
+                                throw err
+                            }
+                            console.log('First name Updated!!!!!')
+                            begin.start()
+                        }
+                    )
+                })
+        }
+    )
+}
+
+function updateEmLast (personId) {
+    begin.con.query(
+        'SELECT * FROM employee', (err, res) => {
+            
+                inquirer.prompt({
+                    type: 'input',
+                    name: 'newLast',
+                    message: 'What is the new last name'
+                })
+                .then(ans => {
+                    begin.con.query(
+                            'UPDATE employee SET ? WHERE ?',
+                            [
+                                {
+                                    last_name: ans.newLast
+                                },
+                                {
+                                    id: personId
+                                }
+                            ],
+                            err => {
+                                if (err) {
+                                    throw err
+                                }
+                                console.log('Last name Updated!!!!!')
+                                begin.start()
+                            }
+                        )
+                })
+            
+        }
+    )
+}
+
+module.exports.updateSomething = updateSomething
