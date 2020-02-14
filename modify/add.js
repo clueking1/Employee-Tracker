512afaaa5d561eeb2d48850c89c0ccf918462319
Will Coan <wcoan300@gmail.com>
2020-02-13 21:22:35 -0500

    able to add a role to the company

--- modify/add.js
+++ modify/add.js
@@ -42,3 +42,58 @@ function department() {
         )
     })
 }
+
+//add a role
+function role() {
+    begin.con.query(
+        'SELECT * FROM departments', (err, res) => {
+            if (err) {
+                throw err
+            }
+            inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: "What is the title of the posistion you'd like to add?"
+                },
+                {
+                    type: 'input',
+                    name: 'sal',
+                    message: 'What is the salary for this title?',
+                    validate: value => !isNaN(value) ? true:false
+                },
+                {
+                    type: 'list',
+                    name: 'depart',
+                    message: 'Which department is this title in?',
+                    choices: res.map(t => t.name)
+                }
+        
+            ])
+            .then(ans => {
+                let chosenDepart;
+
+                res.find(t => {
+                    if (t.name === ans.depart) {
+                        chosenDepart = t.id
+                    }
+                })
+             
+                begin.con.query(
+                    'INSERT INTO role SET ?',
+                    {
+                        title: ans.title,
+                        salary: ans.sal,
+                        department_id: chosenDepart
+                    },
+                    err => {
+                        if (err) {
+                            throw err
+                        }
+                        console.log('Role added!')
+                        begin.start()
+                    }
+                )
+            })
+        })
+    }
